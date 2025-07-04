51b0d2bc3e72c26458aaa81b0777e13f3fdf77b1
tcunha2004 <tcunha2004@gmail.com>
2025-07-04 10:00:30 -0300

    update

--- api/lead.js
+++ api/lead.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: "Bearer ${process.env.RESEND_API_KEY}",
+        Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
       },
       body: JSON.stringify({
         from: "Thiago Cunha <thiago.cunha@mipconstrutora.com.br>",
