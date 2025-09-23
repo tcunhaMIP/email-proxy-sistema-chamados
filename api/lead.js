3dee23912e6ae72f09d35e4876f3e00eb80720d4
tcunha2004 <tcunha2004@gmail.com>
2025-09-23 14:03:19 -0300

    fix: nome remetente

--- api/lead.js
+++ api/lead.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
       },
       // dados
       body: JSON.stringify({
-        from: "Thiago Cunha <thiago.cunha@mipconstrutora.com.br>",
+        from: "Sistema de Chamados <thiago.cunha@mipconstrutora.com.br>",
         to: ["ti@mipconstrutora.com.br"],
         subject: `Novo Chamado de ${data.nomeFunc}`,
         html: `<p>Novo Chamado recebido:<br><br>Nome do Funcionário: ${data.nomeFunc}<br>Departamento: ${data.depFunc}<br>Problema: ${data.problemaFunc}<br>Descrição: ${data.desc}<br></p>`,
