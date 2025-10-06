c76e38ea47496e52ea1b16c5169aaf0227b0a619
tcunha2004 <tcunha2004@gmail.com>
2025-10-06 15:28:06 -0300

    feat: criticidade

--- api/lead.js
+++ api/lead.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
         from: "Sistema de Chamados <thiago.cunha@mipconstrutora.com.br>",
         to: ["ti@mipconstrutora.com.br"],
         subject: `${data.title} de ${data.nomeFunc}`,
-        html: `<p>Novo Chamado recebido:<br><br>Nome do Funcionário: ${data.nomeFunc}<br>Departamento: ${data.depFunc}<br>Problema: ${data.problemaFunc}<br>Descrição: ${data.desc}<br><br>Preferência de contato: ${data.contato}<br></p>`,
+        html: `<p>Novo Chamado recebido:<br><br>Nome do Funcionário: ${data.nomeFunc}<br>Departamento: ${data.depFunc}<br>Problema: ${data.problemaFunc}<br>Descrição: ${data.desc}<br><br>Preferência de contato: ${data.contato}<br>Grau de Criticidade: ${data.criticidade}<br></p>`,
       });
     } else if (data.emailFunc != null) {
       body = JSON.stringify({
